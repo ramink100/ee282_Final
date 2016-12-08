1d83bb968e03cdf2315585337835fd8b9b7f0eba
ramink100 <ramink@uci.edu>
2016-12-08 19:42:57 +0000

    Just minor updates to the notes.

--- code/scripts/create_finalcode.sh
+++ code/scripts/create_finalcode.sh
@@ -3,8 +3,9 @@
 
 
 #module load jje/jjeutils
+#module load jje/kent
 #createProject ee282_final
-#module load brew/git
+#module load brew/git for the updated version of github on the cluster
 #git status
 #git remote add origin http://github.com/ramink100/ee282_Final.git
 #git push -u origin master

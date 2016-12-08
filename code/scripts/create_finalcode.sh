b6804c54fd10086581404676a912eb84940f8f08
ramink100 <ramink@uci.edu>
2016-12-08 18:44:55 +0000

    Beginning placeholder for script with notes

---
+++ code/scripts/create_finalcode.sh
@@ -0,0 +1,12 @@
+#curl ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/current/fasta/dmel-all-chromosome-r6.13.fasta.gz -o dmel-all-chromosome-r6.13.fasta.gz
+
+
+
+#module load jje/jjeutils
+#createProject ee282_final
+module load brew/git
+git status
+git remote add origin http://github.com/ramink100/ee282_Final.git
+git push -u origin master
+
+

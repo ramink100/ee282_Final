60b076007923e947d950814058a533f14cc612d4
ramink100 <ramink@uci.edu>
2017-11-15 18:52:57 -0800

    Update create_finalcode.sh

--- code/scripts/create_finalcode.sh
+++ code/scripts/create_finalcode.sh
@@ -11,5 +11,63 @@
 #git remote add origin http://github.com/ramink100/ee282_Final.git
 #git push -u origin master
 
+faSize dmel-all-chromosome-r6.13.fasta > ~/repos/ee282_final/data/processed/summary
 
+infile=~/repos/ee282_final/data/raw/dmel-all-chromosome-r6.13.fasta
+outname=~/repos/ee282_final/data/processed/dmelrel6_filtered.fa
+faFilter -maxSize=100000 $infile /dev/stdout \
+| tee $outname.fa \
+| faSize -detailed /dev/stdin \
+| sort -rnk 2,2 \
+> $outname.sizes
 
+outname=~/repos/ee282_final/data/processed/dmelrel6_min1000000
+faFilter -minSize=100000 $infile /dev/stdout \
+| tee $outname.fa \
+| faSize -detailed /dev/stdin \
+| sort -rnk 2,2 \
+> $outname.sizes
+
+faSize dmelrel6_min100000.fa > ~/repos/ee282_final/data/processed/summarymin100000
+
+faSize dmelrel6_filtered.fa > ~/repos/ee282_final/data/processed/summarymax100000
+
+bioawk -c fastx '{print ">"$name, gc($seq)}' raw/dmel-all-chromosome-r6.13.fasta > processed/dmelrel6_gc
+
+infile=~/repos/ee282_final/data/raw/dmel-all-chromosome-r6.13.fasta
+outname=~/repos/ee282_final/data/processed/dmelrel6_size
+faSize -detailed $infile |sort -rnk 2,2 > $outname
+
+bioawk -c gff '{print $seqname }' dmel-all-r6.13.gtf.gz | sort | uniq -c | sort -rnk 1,1 > ~/repos/ee282_final/data/processed/dmelallr6_features
+
+bioawk -c gff ' $feature =="gene" { print $seqname }' ~/repos/ee282_final/data/raw/dmel-all-r6.13.gtf.gz | sort | uniq -c | sort -rnk 1,1 > ~/repos/ee282_final/data/processed/dmelallr6_genes_chrom
+
+bioawk -c gff ' $feature =="mRNA" { print $group }' ~/repos/ee282_final/data/raw/dmel-all-r6.13.gtf.gz | perl -F"\t" -lane ' $F[0] =~ /gene_id "(FBgn\d{7})"/; print $1; ' | sort | uniq -c | less | awk ' { print $1 } ' | bioawk -c gff ' $feature =="mRNA" { print $group }' ~/repos/ee282_final/data/raw/dmel-all-r6.13.gtf.gz | perl -F"\t" -lane ' $F[0] =~ /gene_id "(FBgn\d{7})"/; print $1; ' | sort | uniq -c | awk ' { print $1 } ' | sort | uniq -c | sort -nk 2,2 > processed/dmelallr6_transcripts
+
+#Graphing in R:
+#I downloaded the files from the cluster to my computer using WinSCP.
+#library(ggplot2)
+#small<-read.table("dmelrel6_filtered.sizes")
+#big<-read.table("dmelrel6_min100000.sizes")
+#ggsmall <-ggplot(data =small)
+#ggsmall +geom_histogram(mapping = aes(x=V2), bins =100)
+#ggbig <-ggplot(data=big)
+#ggbig +geom_histogram(mapping = aes(x=V2), bins =100)
+
+#gc<-read.table("dmelrel6_gc")
+#gggc <-ggplot(data =gc)
+#gggc +geom_histogram(mapping = aes(x=V2), bins =100)
+
+#size<-read.table("dmelrel6_size")
+#ggplot(size, aes(x=1:1870, y=cumsum(V2))) + geom_line()
+
+#transcripts<-read.table("dmelallr6_transcripts")
+#ggplot(data = transcripts, aes(x = V2, y = V1)) + geom_bar(stat="identity")
+
+#gene<-read.table("dmelallr6_genelength")
+#ggplot(gene, aes(x=V1)) + 
+#    geom_histogram(binwidth=1000) + ylim(0,250)
+
+#exon<-read.table("dmelallr6_exonlength")
+#ggplot(exon, aes(x=V1)) + 
+#    geom_histogram(binwidth=10) + ylim(0,300)
